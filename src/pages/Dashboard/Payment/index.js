c714f78152371f9963964cd34fcf3c6a4432e46b
ronycastroc <castroroony@gmail.com>
2022-12-16 21:38:40 -0300

    style: page title and ticket information

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,39 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
-}
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      <SubTitle variant="h6" color="textSecondary">Ingresso escolhido</SubTitle>
+
+      <TicketWrapper>
+        <TicketInfo variant="subtitle1">Presencial + Com Hotel</TicketInfo>
+        <TicketInfo variant="subtitle1" color="textSecondary">R$ 600</TicketInfo>
+      </TicketWrapper>
+    </>
+    
+  );
+};
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 30px!important;
+`;
+
+const SubTitle = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const TicketWrapper = styled.div`
+  width: 290px;
+  height: 108px;
+  border-radius: 20px;
+  background-color: #FFEED2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TicketInfo = styled(Typography)`
+`;
