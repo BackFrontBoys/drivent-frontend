24a390fe441ec792701d529e0ea41d42404434d9
ronycastroc <castroroony@gmail.com>
2022-12-16 23:33:44 -0300

    feat: ticket information in payment page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,15 +1,41 @@
+import { useEffect } from 'react';
+import useTicket from '../../../hooks/api/useTicket';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
+import { useState } from 'react';
 
 export default function Payment() {
+  const { ticket } = useTicket();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(ticket);
+  }, [ticket]);
+
+  function renderTicketInfo() {
+    if(data?.TicketType.isRemote === false && data?.TicketType.includesHotel === false) {
+      return 'Presencial + Sem Hotel';
+    }
+
+    if(data?.TicketType.isRemote === false && data?.TicketType.includesHotel === true) {
+      return 'Presencial + Com Hotel';
+    }
+
+    return 'Online';
+  }
+
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
       <SubTitle variant="h6" color="textSecondary">Ingresso escolhido</SubTitle>
 
       <TicketWrapper>
-        <TicketInfo variant="subtitle1">Presencial + Com Hotel</TicketInfo>
-        <TicketInfo variant="subtitle1" color="textSecondary">R$ 600</TicketInfo>
+        <TicketInfo variant="subtitle1">{renderTicketInfo()}</TicketInfo>
+        <TicketInfo variant="subtitle1" color="textSecondary">
+          R$ {data?.TicketType.price
+            .toString()
+            .slice(0, -2)}
+        </TicketInfo>
       </TicketWrapper>
     </>
     
