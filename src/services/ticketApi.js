53ba9338a9f4b9941ff80374a23c335f2b44ef6f
sanditomaz <sandi.contato@gmail.com>
2022-12-22 02:04:45 -0300

    chore: api requests implemented

--- src/services/ticketApi.js
+++ src/services/ticketApi.js
@@ -8,3 +8,21 @@ export async function getTickets(token) {
   });
   return response.data;
 }
+
+export async function getTicketTypes(token) {
+  const response = await api.get('/tickets/types', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function postTicket(body, token) {
+  const response = await api.post('/tickets', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
