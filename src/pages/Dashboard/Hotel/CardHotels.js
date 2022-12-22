6697d77694dce2430316b62aa83603f7c9fc8b2e
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-12-22 15:05:49 -0300

    feat: hotelsCards informations

--- src/pages/Dashboard/Hotel/CardHotels.js
+++ src/pages/Dashboard/Hotel/CardHotels.js
@@ -1,12 +1,71 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-export function CardHotels({ image, name, id, selectHotel }) {
-  const notFound = 'https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg';
+export function CardHotels({ image, name, id, Rooms, selectHotel }) {
+  const [text, setText] = useState('');
+  const [free, setfree] = useState(0);
+
+  function defineText(Rooms) {
+    let options = {
+      single: false,
+      double: false,
+      triple: false
+    };
+
+    let booking = 0;
+    let aux = 0;
+
+    for(let i = 0; i < Rooms.length; i++) {
+      booking += Number(Rooms[i].Booking.length);
+      aux += Number(Rooms[i].capacity);
+      if(Rooms[i].capacity === 1 && options.single === false) {
+        options.single = true;
+      }
+      if(Rooms[i].capacity === 2 && options.double === false) {
+        options.double = true;
+      }
+      if(Rooms[i].capacity === 3 && options.triple === false) {
+        options.triple = true;
+      }
+    }
+    setfree(aux - booking);
+    if(options.single === true) {
+      setText('Single');
+    }
+    if(options.double === true) {
+      setText('Double');
+    }
+    if(options.triple === true) {
+      setText('Triple');
+    }
+    if(options.single === true && options.double === true) {
+      setText('Single e Double');
+    }
+    if(options.single === true && options.triple === true) {
+      setText('Single e Triple');
+    }
+    if(options.double === true && options.triple === true) {
+      setText('Double e Triple');
+    }
+    if(options.single === true && options.double === true && options.triple === true) {
+      setText('Single, Double e Triple');
+    }
+  }
+
+  useEffect(() => {
+    defineText(Rooms);
+  }, []);
 
   return(
     <Container onClick={() => {selectHotel(id);}}>
-      <img src={image || notFound} alt='hotel'/>
-      <h5>{name || 'teste'}</h5>
+      <img src={image} alt='hotel'/>
+      <h5>{name}</h5>
+      <Information>
+        <h6>Tipos de acomodação:</h6>
+        <p>{text}</p>
+        <h6>Vagas disponíveis:</h6>
+        <p>{free}</p>
+      </Information>
     </Container>
   );
 }
@@ -44,4 +103,38 @@ const Container = styled.div`
         word-wrap: break-word;
     }
 
+`;
+
+const Information = styled.div`
+    width: 196px;
+    height: 264px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: 0px 14px;
+    margin-top: 10px ;
+
+    h6{
+        width: 100%;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        font-size: 12px;
+        line-height: 14px;
+        color: #3C3C3C;
+        word-wrap: break-word;
+    }
+
+    p{
+        width: 100%;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        color: #3C3C3C;
+        word-wrap: break-word;
+    }
+    p:nth-child(2){
+      margin-bottom: 14px;
+    }
 `;
