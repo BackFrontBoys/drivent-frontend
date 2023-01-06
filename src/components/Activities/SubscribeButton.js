f3f4763b4e270922ed5317a76cb3e7c6d5258899
Vinicius <vianaxd99@gmail.com>
2023-01-06 16:26:23 -0300

    style: activities subscribe button

---
+++ src/components/Activities/SubscribeButton.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+
+import { green, red } from '@mui/material/colors';
+
+export default function SubscribeButton({ isSubscribed, availableSpots }) {
+  return (
+    <>
+      <Container>
+        {
+          availableSpots ?
+            (
+              isSubscribed ?
+                (
+                  <>
+                    <LoginOutlinedIcon sx={{ fontSize: 30, color: green[600] }} />
+                    <Spots color={availableSpots}>{availableSpots} vagas</Spots>
+                  </>
+                )
+                :
+                (
+                  <>
+                    <CheckCircleOutlinedIcon sx={{ fontSize: 30, color: green[600] }} />
+                    <Spots color={availableSpots}>Inscrito</Spots>
+                  </>
+                )
+            )
+            :
+            <>
+              <HighlightOffRoundedIcon sx={{ fontSize: 30, color: red[600] }} />
+              <Spots color={availableSpots}>Esgotado</Spots>
+            </>
+        }
+      </Container>
+    </>);
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Spots = styled.p`  
+  font-size: 10px;
+  color: ${props => (props.color ? '#43a047' : '#e93535')};
+
+  font-family: 'Roboto', sans-serif;
+`;
