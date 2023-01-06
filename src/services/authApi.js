4029ab1be8db46840eda78e49fb94700d91c91ed
Vinicius <vianaxd99@gmail.com>
2023-01-06 06:03:52 -0300

    feat: github login

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,4 +4,11 @@ export async function signIn(email, password) {
   const response = await api.post('/auth/sign-in', { email, password });
   return response.data;
 }
+
+export async function gitSignIn(code) {
+  console.log(code);
+  const response = await api.post(`/auth/sign-in/github?code=${code}`);
+  
+  return response.data;
+}
 //
